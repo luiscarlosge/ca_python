0e25f8cadb940ac46ac84c4f97e260ad2bcd2b00
Luis Galvis <luis.galvis@holcim.com>
2024-04-04 11:30:58 -0500

    Initial Commit  Block Letters Module 1

---
+++ M1-P1/initials.py
@@ -0,0 +1,38 @@
+letter_l = """
+L
+L
+L
+L
+L
+L
+LLLLL
+"""
+letter_c = """
+CCCCC
+C
+C
+C
+C
+C
+CCCCC
+"""
+
+letter_g = """
+GGGGG
+G
+G
+G GGG  
+G   G
+G   G
+GGGGG
+"""
+letter_a = """
+  A
+ A A
+A   A
+AAAAA
+A   A
+A   A
+A   A
+"""
+print(letter_l+letter_c+letter_g+letter_a)
