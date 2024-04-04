08094015a67aa1c5a74e237b5b319e4fac81dc53
Luis Galvis <luis.galvis@holcim.com>
2024-04-04 11:38:19 -0500

    Fix on output

--- M1-P1/initials.py
+++ M1-P1/initials.py
@@ -1,38 +1,15 @@
-letter_l = """
-L
-L
-L
-L
-L
-L
-LLLLL
-"""
-letter_c = """
-CCCCC
-C
-C
-C
-C
-C
-CCCCC
-"""
+line1 = "L     CCCCC GGGGG EEEEE"
+line2 = "L     C     G     E    "
+line3 = "L     C     G     E    "
+line4 = "L     C     GGGGG EEEEE"
+line5 = "L     C     G   G E    "
+line6 = "L     C     G   G E    "
+line7 = "LLLLL CCCCC GGGGG EEEEE" 
 
-letter_g = """
-GGGGG
-G
-G
-G GGG  
-G   G
-G   G
-GGGGG
-"""
-letter_a = """
-  A
- A A
-A   A
-AAAAA
-A   A
-A   A
-A   A
-"""
-print(letter_l+letter_c+letter_g+letter_a)
+print (line1)
+print (line2)
+print (line3)
+print (line4)
+print (line5)
+print (line6)
+print (line7)
